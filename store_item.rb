0085636265ca8933e9161d58745326440deba0e8
Sara Raley <sara.m.raley@gmail.com>
2022-10-25 13:12:32 -0600

    adds closing punctuation

--- store_item.rb
+++ store_item.rb
@@ -2,4 +2,4 @@ sandwiches = { "color" => "red", "price" => 30, "stars" => 5 }
 soups = { color: "green", price: 14, stars: 2 }
 salads = { :color => "blue", :price => 3, :stars => 3 }
 
-puts "The best store sells #{sandwiches["color"]} sandwiches, salads for #{salads[:price]} dollars, and #{soups[:stars]}-star soups"
+puts "The best store sells #{sandwiches["color"]} sandwiches, salads for #{salads[:price]} dollars, and #{soups[:stars]}-star soups."
