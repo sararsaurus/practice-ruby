69ffe73c91d999467bbb85fcac980b859630cd22
Sara Raley <sara.m.raley@gmail.com>
2022-10-29 08:12:08 -0600

    changed while loop to times loop for user inputs

--- ruby_review.rb
+++ ruby_review.rb
@@ -41,27 +41,31 @@
 
 #   Gertrude Bell gb@gmail.com ]
 
-# user_inputs = []
-# banking_info = []
-# index = 0
-# while index < 5
-#   puts "Enter a first name, last name, and email"
-#   info = gets.chomp
-#   user_inputs = info.split
-#   banking_info << user_inputs
-#   index += 1
-# end
-banking_info = [["Marco", "Polo", "mpolo@gmail.com"], ["Ibn", "Battuta,", "ib@gmail.com"], ["Leif", "Erikson", "le@gmail.com"], ["Amelia", "Earhart", "ae@gmail.com"], ["Gertrude", "Bell", "gb@gmail.com"]]
+user_inputs = []
+banking_info = []
+5.times do
+  puts "Enter a first name, last name, and email"
+  info = gets.chomp
+  user_inputs = info.split
+  banking_info << user_inputs
+end
+
+# banking_info = [["Marco", "Polo", "mpolo@gmail.com"], ["Ibn", "Battuta,", "ib@gmail.com"], ["Leif", "Erikson", "le@gmail.com"], ["Amelia", "Earhart", "ae@gmail.com"], ["Gertrude", "Bell", "gb@gmail.com"]]
 
 fields = [:first_name, :last_name, :email]
 
 array_of_hashes = banking_info.map { |row| fields.zip(row).to_h }
 # p array_of_hashes
 
-account_number = rand(10 ** 10)
+# account_number = rand(10 ** 10)
 # p account_number
 
 array_of_hashes.each do |hash|
   hash[:account_number] = rand(10 ** 10)
 end
 p array_of_hashes
+
+# banking_info = [{ :first_name => "Marco", :last_name => "Polo", :email => "mpolo@gmail.com", :account_number => 859939824 }, { :first_name => "Ibn", :last_name => "Battuta,", :email => "ib@gmail.com", :account_number => 3354679702 }, { :first_name => "Leif", :last_name => "Erikson", :email => "le@gmail.com", :account_number => 6990759058 }, { :first_name => "Amelia", :last_name => "Earhart", :email => "ae@gmail.com", :account_number => 7668425294 }, { :first_name => "Gertrude", :last_name => "Bell", :email => "gb@gmail.com", :account_number => 6099546618 }]
+
+# puts "What's your account number?"
+# account = gets.chomp.to_i
